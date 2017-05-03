4e656f53362c61eba5f5a38ccaa3560d8f274284
insomniac0 <insomniacmendo@gmail.com>
2017-05-03 10:37:25 -0700

    Update and rename packet.hpp to fwd/packet.hpp

--- fwd/packet.hpp
+++ fwd/packet.hpp
@@ -1,9 +1,3 @@
-
-/* $Id: packet.hpp 168 2009-10-23 14:58:32Z sausage $
- * EOSERV is released under the zlib license.
- * See LICENSE.txt for more info.
- */
-
 #ifndef FWD_PACKET_HPP_INCLUDED
 #define FWD_PACKET_HPP_INCLUDED
 
